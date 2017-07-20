845d848560b43bddca11f42d8cfa253a6408d0a1
Sasha Lopoukhine <superlopuh@gmail.com>
2017-07-20 14:36:14 +0200

    use start instead of constructors

--- index.js
+++ index.js
@@ -2,13 +2,18 @@
 
 var mqtt_ble = require('./mqtt_ble_relay');
 
-var relayConfig = {
-    displayName : 'Snips',
-    mqttURL : 'localhost',
-    mqttPort : '9898',
-    serviceUUID : '025A7775-49AA-42BD-BBDB-E2AE77782966',
-    characteristicUUID : 'F38A2C23-BC54-40FC-BED0-60EDDA139F47'
+
+var config = {
+    mqtt: {
+        host: 'localhost',
+        port: '9898'
+    },
+    ble: {
+        displayName : 'Snips',
+        serviceUUID : '025A7775-49AA-42BD-BBDB-E2AE77782966',
+        characteristicUUID : 'F38A2C23-BC54-40FC-BED0-60EDDA139F47'
+    }
 }
 
-var relay = new mqtt_ble.Relay(relayConfig);
-relay.start();
+
+mqtt_ble.start(config);
--- mqtt_ble_relay.js
+++ mqtt_ble_relay.js
@@ -3,47 +3,37 @@
 var mqtt = require('mqtt');
 var ble = require('./ble_relay');
 
-class Relay {
-	constructor(config) {
-		var client = mqtt.connect({host: config.mqttURL, port: config.mqttPort});
 
-		var onMessage = function(buffer) {
-			var json = JSON.parse(buffer);
-			var message = new Buffer(json.message, 'base64');
-			client.publish(json.topic, message);
-		}
+var start = function(config) {
+	var client = mqtt.connect(config.mqtt);
 
-		var getReadData = function() { return new Buffer('Read data'); }
+	config.ble.onMessage = function(buffer) {
+		var json = JSON.parse(buffer);
+		var message = new Buffer(json.message, 'base64');
+		client.publish(json.topic, message);
+	};
 
-		var bleRelay = new ble.Relay(onMessage, getReadData);
+	config.ble.getReadData = function() { 
+		return new Buffer('Read data'); 
+	};
 
-		client.on('message', function (topic, message) {
-		    // message is Buffer
-		    var json = {
-		        'topic': topic,
-		        'message': message.toString('base64')
-		    }
-		    var text = JSON.stringify(json);
-		    var buff = new Buffer(text);
-		    bleRelay.sendMessage(buff);
-		});
+	var bleRelay = ble.start(config.ble);
 
-		this.config = config;
-		this.client = client;
-		this.bleRelay = bleRelay;
-	}
+	client.on('message', function (topic, message) {
+	    // message is Buffer
+	    var json = {
+	        'topic': topic,
+	        'message': message.toString('base64')
+	    }
+	    var text = JSON.stringify(json);
+	    var buff = new Buffer(text);
+	    bleRelay.sendMessage(buff);
+	});
 
-	start() {
-		var client = this.client;
-		this.client.on('connect', function () {
-		  	client.subscribe('#');
-		});
-		this.bleRelay.start(
-			this.config.displayName,
-			this.config.serviceUUID,
-			this.config.characteristicUUID
-		);
-	}
-}
+	client.on('connect', function () {
+	  	client.subscribe('#');
+	});
+};
 
-module.exports = {Relay: Relay};
+
+module.exports = {start: start};
